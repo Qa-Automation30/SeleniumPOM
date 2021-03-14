3f7dfda10ed220c8da3bcf5a58673354bcd3c732
Qa-Automation30 <qaautomation30@gmail.com>
2021-03-14 13:05:36 +0530

    changes has been done

--- src/main/java/com/test/BaseClass/BaseClass.java
+++ src/main/java/com/test/BaseClass/BaseClass.java
@@ -10,40 +10,42 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class BaseClass {
 	public static WebDriver driver;
-	public static Properties prop;
+	public static Properties prop = new Properties();
 	
 	public static void loadPropertyFile()
 	{
 		try {
 			FileInputStream fis = new FileInputStream(System.getProperty("user.dir")+"/src/main/java/com/test/configFile/config.properties");
-			prop.load(fis);
+			try {
+				prop.load(fis);
+			} catch (IOException e) {
+				System.out.println(e.toString());
+			}
 		} catch (FileNotFoundException e) {
-			System.out.println(e.toString());
-		} catch (IOException e) {
 			System.out.println(e.toString());
 		}
 	}
-	public static WebDriver setUpWebDriver(String browser,String url)
+	public static WebDriver setUpWebDriver()
 	{
 		loadPropertyFile();
-		if(browser.equalsIgnoreCase("chrome"))
+		if(prop.getProperty("browser").equalsIgnoreCase("chrome"))
 		{
 		System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"/src/chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-		driver.get(url);
+		driver.get(prop.getProperty("url"));
 		return driver;
 		}
-		else if(browser.equalsIgnoreCase("firefox"))
+		else if(prop.getProperty("browser").equalsIgnoreCase("firefox"))
 		{
 			System.setProperty("webdriver.gecko.driver",System.getProperty("user.dir")+"/src/geckodriver.exe");
 			driver = new FirefoxDriver();
 			driver.manage().window().maximize();
 			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 			driver.manage().deleteAllCookies();
-			driver.get(url);
+			driver.get("url");
 			return driver;
 		}
 		return null;
--- src/test/java/com/test/testLayers/LoginTest.java
+++ src/test/java/com/test/testLayers/LoginTest.java
@@ -1,5 +1,6 @@
 package com.test.testLayers;
 
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -13,7 +14,7 @@ public class LoginTest extends BaseClass {
 	@BeforeMethod
 	public void setUp()
 	{
-		setUpWebDriver(prop.getProperty("browser"), prop.getProperty("url"));
+		setUpWebDriver();
 		loginrepo = new LoginPageRepo();
 	}
 	
